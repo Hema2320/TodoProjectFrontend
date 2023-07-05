ee7a71849ef61f1e238c36a2c36134b6ca89a2f0
Hema2320 <hemadharsh23@gmail.com>
2023-07-05 09:36:35 +0530

    Frontend

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+       <><><h1>THE NOTES TAKER</h1></>
+        <header>
+            <h2>Take your Notes here</h2>
+        </header></>
+    )
+}
+
+export default Header;
